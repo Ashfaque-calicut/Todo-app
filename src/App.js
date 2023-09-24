b35bd103020635c4459cea3d5aafe9a955afd173
Ashfaque-calicut <ashfaquepalazhi@gmail.com>
2023-09-25 00:42:16 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import React from 'react';
-import {useState} from 'react'
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-  const [toDos,setToDos]=useState([])
-  const [toDo,setToDo]= useState('')
+  const [toDos, setToDos] = useState([]);
+  const [toDo, setToDo] = useState('');
+
+  // Function to handle the click event on the "fas fa-times" icon
+  const handleRemoveTodo = (id) => {
+    const updatedToDos = toDos.filter((item) => item.id !== id);
+    setToDos(updatedToDos);
+  };
+
   return (
     <div className="app">
       <div className="mainHeading">
@@ -12,43 +18,65 @@ function App() {
       </div>
       <div className="subHeading">
         <br />
-        <h2>Whoop, it's Wednesday 🌝 ☕ </h2>
+        <h2>Whoop, it's a Good Day 🌝 ☕ </h2>
       </div>
       <div className="input">
-        <input value={toDo} onChange={(e)=>setToDo(e.target.value)} type="text" placeholder="🖊️ Add item..." />
-        <i onClick={()=>setToDos([...toDos,{id:Date.now() ,text:toDo, status:false}])} className="fas fa-plus"></i>
+        <input
+          value={toDo}
+          onChange={(e) => setToDo(e.target.value)}
+          type="text"
+          placeholder="🖊️ Add item..."
+        />
+        <i
+          onClick={() =>
+            setToDos([
+              ...toDos,
+              { id: Date.now(), text: toDo, status: false },
+            ])
+          }
+          className="fas fa-plus"
+        ></i>
       </div>
       <div className="todos">
-       { toDos.map((obj)=>{
-      return( <div className="todo">
-          <div className="left">
-            <input onChange={(e)=>{
-              console.log(e.target.checked)
-              console.log(obj)
-              setToDos(toDos.filter(obj2=>{
-                if(obj2.id===obj.id){
-                  obj2.status=e.target.checked
-                }
-                return obj2
-              }))
-
-            }} value={obj.status} type="checkbox" name="" id="" />
-            <p>{obj.text}</p>
-          </div>
-          <div className="right">
-            <i className="fas fa-times"></i>
-          </div>
-        </div>)}) }
-        {toDos.map((obj)=>{
-          if(obj.status){
-            return(<h1>{obj.text}</h1>)
-          }
-          return null
+        {toDos.map((obj) => {
+          return (
+            <div className="todo" key={obj.id}>
+              <div className="left">
+                <input
+                  onChange={(e) => {
+                    const updatedToDos = toDos.map((obj2) => {
+                      if (obj2.id === obj.id) {
+                        obj2.status = e.target.checked;
+                      }
+                      return obj2;
+                    });
+                    setToDos(updatedToDos);
+                  }}
+                  value={obj.status}
+                  type="checkbox"
+                  name=""
+                  id=""
+                />
+                <p>{obj.text}</p>
+              </div>
+              <div className="right">
+                <i
+                  className="fas fa-times"
+                  onClick={() => handleRemoveTodo(obj.id)}
+                ></i>
+              </div>
+            </div>
+          );
         })}
-
+        {toDos.map((obj) => {
+          if (obj.status) {
+            return <h1 key={obj.id}>{obj.text}</h1>;
+          }
+          return null;
+        })}
       </div>
     </div>
   );
-};
+}
 
 export default App;
